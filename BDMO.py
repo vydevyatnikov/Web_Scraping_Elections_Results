ecc86c70f7990e0ae6aaf6d04088f4b22407f2cf
Vadim Y. Devyatnikov <68634043+vydevyatnikov@users.noreply.github.com>
2022-04-13 12:00:11 +0300

    Added the ability to extract data.

--- BDMO.py
+++ BDMO.py
@@ -29,7 +29,7 @@ class Container:
             #self.driver.get(regions_url[[j for j, z in enumerate(regions_names) if z == i][0]])
             self.driver.get(regions_url[regions_names.index(i)])
             self.driver.find_element_by_xpath("//input[@id='Knopka2']").click()
-            region_data = RaionsPage(self.years, self.indicators, self.driver, region=i)
+            self.data = self.data.append(RaionsPage(self.years, self.indicators, self.driver, region=i).data)
 
 
     @staticmethod
@@ -51,11 +51,12 @@ class RaionsPage:
         self.links = self.find_links()
         self.region = region
 
-        self.choose_indicators()
-        self.get_the_data()
+        for raion in self.links.keys():
+            self.choose_indicators(raion)
+            self.get_the_data(raion)
 
     def find_links(self):
-        links = []
+        links = {}
         banned_words = ["районы", "городские округа", "область", "край", "республика", " округ "]
         divs = self.driver.find_elements_by_xpath("//div[@id='WebTree']/div")
         div_id = [el.get_attribute("id") for el in divs]
@@ -71,8 +72,11 @@ class RaionsPage:
                 else:
                     continue
             if len(div_id[num-1]) == 0:
-                links.append(self.driver.find_element_by_xpath(f"//div[@id='WebTree']/div[{num}]/a").
-                             get_attribute("href"))
+                links[self.driver.find_element_by_xpath(
+                    f"//div[@id='WebTree']/div[{num}]/a").text] = self.driver.find_element_by_xpath(
+                    f"//div[@id='WebTree']/div[{num}]/a").get_attribute("href")
+                #links.append(self.driver.find_element_by_xpath(f"//div[@id='WebTree']/div[{num}]/a").
+                #             get_attribute("href"))
             elif "title" in div_id[num-1]:
                 divs[num - 1].click()
                 if cities:
@@ -81,42 +85,49 @@ class RaionsPage:
                     cities = False
                 continue
             else:
-                [links.append(self.driver.find_element_by_xpath(f"//div[@id='WebTree']/div[{num}]/div[{j}]/a").
-                              get_attribute("href")) for j in sub_rows]
+                links = {**links, **{self.driver.find_element_by_xpath(f"//div[@id='WebTree']/div[{num}]/div[{j}]/a").
+                                         text: self.driver.find_element_by_xpath(
+                    f"//div[@id='WebTree']/div[{num}]/div[{j}]/a").get_attribute("href") for j in sub_rows}}
                 sub_rows = [1]
         return links
 
-    def choose_indicators(self):
-        for link in self.links:
-            self.driver.get(link)
-            years_boxes = np.array(self.driver.find_elements_by_xpath("//table[@id='yearlist']/tbody/tr/td/input"))
-            years_texts = np.array([int(el.text) for el in self.driver.find_elements_by_xpath(
-                "//table[@id='yearlist']/tbody/tr/td")])
-            for year in years_boxes[years_texts >= 2013]:
-                year.click()
-            self.driver.find_element_by_xpath("//table[@class='tbl']/tbody/tr[10]/td/input").click()
-            indicators_types_texts = [el.text.lower() for el in self.driver.find_elements_by_xpath(
-                "//table[@class='tbl']/tbody/tr[10]/td/span/div/span/span[2]")]
-            indicators_types_num = [i for i, z in enumerate(indicators_types_texts) if
-                                    "местного самоуправления" in z][0]
-            self.driver.find_elements_by_xpath(
-                "//table[@class='tbl']/tbody/tr[10]/td/span/div/span/span[2]")[indicators_types_num].click()
-            indicator_menu_text = [el.text.lower() for el in self.driver.find_elements_by_xpath(
-                f"//table[@class='tbl']/tbody/tr[10]/td/span/div[{indicators_types_num+1}]/div/div/a")]
-            indicator_menu = self.driver.find_elements_by_xpath(
-                f"//table[@class='tbl']/tbody/tr[10]/td/span/div[{indicators_types_num+1}]/div/div/a/input")
-            for i in range(len(indicator_menu_text)):
-                if sum([1 if ind in indicator_menu_text[i] else 0 for ind in self.indicators]) > 0:
-                    indicator_menu[i].click()
-            self.driver.find_element_by_xpath("//td[@class='buttons']/input[@name='Button_Table']").click()
+    def choose_indicators(self, raion):
+        self.driver.get(self.links[raion])
+        years_boxes = np.array(self.driver.find_elements_by_xpath("//table[@id='yearlist']/tbody/tr/td/input"))
+        years_texts = np.array([int(el.text) for el in self.driver.find_elements_by_xpath(
+            "//table[@id='yearlist']/tbody/tr/td")])
+        for year in years_boxes[years_texts >= 2013]:
+            year.click()
+        self.driver.find_element_by_xpath("//table[@class='tbl']/tbody/tr[10]/td/input").click()
+        indicators_types_texts = [el.text.lower() for el in self.driver.find_elements_by_xpath(
+            "//table[@class='tbl']/tbody/tr[10]/td/span/div/span/span[2]")]
+        indicators_types_num = [i for i, z in enumerate(indicators_types_texts) if
+                                "местного самоуправления" in z][0]
+        self.driver.find_elements_by_xpath(
+            "//table[@class='tbl']/tbody/tr[10]/td/span/div/span/span[2]")[indicators_types_num].click()
+        indicator_menu_text = [el.text.lower() for el in self.driver.find_elements_by_xpath(
+            f"//table[@class='tbl']/tbody/tr[10]/td/span/div[{indicators_types_num+1}]/div/div/a")]
+        indicator_menu = self.driver.find_elements_by_xpath(
+            f"//table[@class='tbl']/tbody/tr[10]/td/span/div[{indicators_types_num+1}]/div/div/a/input")
+        for i in range(len(indicator_menu_text)):
+            if sum([1 if ind in indicator_menu_text[i] else 0 for ind in self.indicators]) > 0:
+                indicator_menu[i].click()
+        self.driver.find_element_by_xpath("//td[@class='buttons']/input[@name='Button_Table']").click()
         return self
 
-    def get_the_data(self):
-            years = pd.Series([int(el.text) for el in self.driver.find_elements_by_xpath(
-                "//table[@class='passport']/tbody/tr[1]/th")[2:]])
-            temp_data = pd.DataFrame({"year": years, "region": iter.repeat(self.region, len(years))})
-            for j in range(2, len(self.driver.find_elements_by_xpath("//table[@class='passport']/tbody/tr")) + 1):
-                print("smth")
+    def get_the_data(self, raion):
+        years = pd.Series([el.text for el in self.driver.find_elements_by_xpath(
+            "//table[@class='passport']/tbody/tr[1]/th")[2:]], dtype="int")
+        temp_data = pd.DataFrame({**{"year": years, "region": pd.Series(iter.repeat(self.region, len(years))),
+                                     "raion": pd.Series(iter.repeat(raion, len(years)))},
+                                 **{i: iter.repeat(np.NaN, len(years)) for i in self.indicators}})
+        for j in range(2, len(self.driver.find_elements_by_xpath("//table[@class='passport']/tbody/tr")) + 1):
+            indicator = self.driver.find_element_by_xpath(f"//table[@class='passport']/tbody/tr[{j}]/td[1]").text
+            temp_data[self.indicators[[i for i, z in enumerate(self.indicators) if z in indicator][0]]] = [
+                float(el.text) if len(el.text) > 0 else np.NaN for el in self.driver.find_elements_by_xpath(
+                    f"//table[@class='passport']/tbody/tr[{j}]/td[position()>2]")]
+        self.data = self.data.append(temp_data)
+
 
 
 
@@ -133,7 +144,7 @@ class RaionData:
 
 
 if __name__ == "__main__":
-    Container(regions=["Приморский край"], indicators=[
+    Container(regions=["Амурская область"], indicators=[
         "автодорог", "автобусного", "аварийном", "незавершенного", "жилищные"], years=["years"])
 
 # ["Приморский край", "Амурская область", "Кировская область", "Липецкая область", "Мурманская область",
