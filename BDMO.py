931c13af6359b759e6025a18d7a78f97ab98bbe4
Vadim Y. Devyatnikov <68634043+vydevyatnikov@users.noreply.github.com>
2023-02-25 19:14:48 +0300

    A little bit cleaner

--- BDMO.py
+++ BDMO.py
@@ -24,7 +24,7 @@ class Container:
             self.initiate_process()
         finally:
             breakpoint()
-            # self.data.to_csv("D:/DZ/Course_5/Курсовая/data/BDMO/overall.csv")
+            # self.data.to_csv("D:/DZ/Course_6/Diploma/Data/Financial_data/script/overall.csv")
             self.driver.quit()
 
     def initiate_process(self):
@@ -66,6 +66,12 @@ class RaionsPage:
             # self.get_the_data(raion)
         self.data.to_csv(f"D:/DZ/Course_6/Diploma/Data/Financial_data/script/{region}.csv")
 
+    def get_indicators(self):
+        for ind in self.indicators.keys():
+            for sub_ind in self.indicators[ind].keys():
+                for time in self.indicators[ind][sub_ind]:
+                    yield [ind, sub_ind, time]
+
     def find_links(self, banned_words, num=None, path="//div[@id='WebTree']"):
         divs = self.driver.find_elements_by_xpath(path + "/div")
         div_id = [el.get_attribute("id") for el in divs]
@@ -97,7 +103,9 @@ class RaionsPage:
         years_boxes = np.array(self.driver.find_elements_by_xpath("//table[@id='yearlist']/tbody/tr/td/input"))
         years_texts = np.array([int(el.text) for el in self.driver.find_elements_by_xpath(
             "//table[@id='yearlist']/tbody/tr/td")])
-        for year in years_boxes[years_texts >= 2013]:  # поменять на years
+        needed_years = (years_texts >= 2013) & (years_texts <= 2022)
+        present_years = years_texts[needed_years]
+        for year in years_boxes[needed_years]:  # поменять на years?
             year.click()
         self.driver.find_element_by_xpath("//table[@class='tbl']/tbody/tr[10]/td/input").click()
         indicators_types_texts = [el.text.lower() for el in self.driver.find_elements_by_xpath(
@@ -110,9 +118,7 @@ class RaionsPage:
         except IndexError:
             return
 
-        how_many_types = len(indicators_types_num)
-
-        for j in range(how_many_types):
+        for j in range(len(indicators_types_num)):
             ind_num = indicators_types_num[j]
             self.driver.find_elements_by_xpath(
                 "//table[@class='tbl']/tbody/tr[10]/td/span/div/span/span[2]")[ind_num].click()
@@ -135,7 +141,7 @@ class RaionsPage:
 
         data = pd.DataFrame()
 
-        # creates dataframe, which basically replicates data presented on the site
+        # creates dataframe, which basically replicates data present on the site (replace with pd.read_html?)
         for tp in range(len(self.driver.find_elements_by_xpath("//table[@class='passport']"))):
             elems = self.driver.find_elements_by_xpath(
                 f"//table[@class='passport' and position()={tp + 1}]/tbody/tr[position() > 1]/td[1]")
@@ -143,16 +149,18 @@ class RaionsPage:
                                     "style": pd.Series([style if len(style) != 0 else "absent"
                                                         for style in [el.get_attribute("style") for el in elems]]),
                                     "text": pd.Series([el.text.lower() for el in elems])})
-            for num in range(len(self.years)):
-                temp_df[self.years[num]] = pd.Series(float(el.text) if len(el.text) > 0 else np.NaN
-                                                     for el in self.driver.find_elements_by_xpath(
+            for num in range(len(present_years)):
+                temp_df[present_years[num]] = pd.Series(float(el.text) if len(el.text) > 0 else np.NaN
+                                                        for el in self.driver.find_elements_by_xpath(
                     f"//table[@class='passport' and position()={tp + 1}]/tbody/tr[position() > 1]/td[{num + 3}]"))
+            temp_df["unit"] = [el.text for el in self.driver.find_elements_by_xpath(
+                f"//table[@class='passport' and position()={tp + 1}]/tbody/tr[position() > 1]/td[2]")]
             data = data.append(temp_df)
         data.reset_index(inplace=True, drop=True)
 
+        # do we need it?
         indices = np.array([len(data)])
-
-        data["indicator"] = pd.Series(itertools.repeat("Not present", len(data)))
+        data["indicator"] = pd.Series(itertools.repeat("Not present", len(data)))  # заменить на [] * n
         temp_series = data.loc[(data["class"] == "pok") & (data["style"] == "absent"), "text"]
         for g in temp_series.index:
             indic_temp = [m for m in self.alias_dict.keys() if m == temp_series[g]]
@@ -167,6 +175,12 @@ class RaionsPage:
 
         data["sub_indicator"] = pd.Series(itertools.repeat("Not present", len(data)))
         temp_series = data.loc[(data["class"] == "prizn") & (data["style"] == "padding-left: 10pt;"), "text"]
+        for g in temp_series.index:
+            indic_temp = [m for m in self.alias_dict.keys() if m == temp_series[g]]
+            if len(indic_temp) == 1:
+                temp_series.loc[g] = self.alias_dict[indic_temp[0]]
+            #else:
+            #    breakpoint()
         indices = np.concatenate((indices, np.array(temp_series.index)))
         for i in range(len(temp_series)):
             data.loc[temp_series.index[i]:indices[(indices - temp_series.index[i]) > 0].min() - 1,
@@ -178,70 +192,48 @@ class RaionsPage:
         for i in range(len(temp_series)):
             data.loc[temp_series.index[i]:indices[(indices - temp_series.index[i]) > 0].min() - 1,
                      "times"] = self.alias_dict[temp_series.iloc[i]]
-        print("Done")
 
-        years = pd.Series([el.text for el in self.driver.find_elements_by_xpath(
-            "//table[@class='passport' and position()=1]/tbody/tr[1]/th")[2:]], dtype="int")
+        temp_data = pd.DataFrame({"year": present_years,
+                                  "region": pd.Series(itertools.repeat(self.region, len(present_years))),
+                                  "raion": pd.Series(itertools.repeat(raion, len(present_years)))})
 
-        data_dict = {i:
-                     {j:
-                      {g:
-                       data.loc[(data.indicator == i) & (data.sub_indicator == j) &
-                                (data.times == g)].iloc[0, 3:len(years) + 3]
-                       for g in data.loc[(data.indicator == i) & (data.sub_indicator == j)].times.unique()}
-                      for j in data.loc[data.indicator == i].sub_indicator.unique()}
-                     for i in data.indicator.unique()}
-        temp_data = pd.DataFrame({**{"year": years, "region": pd.Series(itertools.repeat(self.region, len(years))),
-                                     "raion": pd.Series(itertools.repeat(raion, len(years)))},
-                                  **{i: itertools.repeat(np.NaN, len(years)) for i in self.indicators.keys()}})
-
-        for ind in self.indicators.keys():
-            multiple_subs = False
-            try:
-                temp_ind = data_dict[ind]["Not present"]["Not present"]
-            except KeyError:
-                continue
-            if len(self.indicators[ind]) != 0:
-                if len(self.indicators[ind]) > 1:
-                    multiple_subs = True
-                for sub_ind in self.indicators[ind].keys():
-                    try:
-                        temp_sub_ind = data_dict[ind][sub_ind]["Not present"]
-                    except KeyError:
-                        continue
-                    temp_ind.loc[~pd.isnull(temp_sub_ind)] = temp_sub_ind.loc[~pd.isnull(temp_sub_ind)]
-                    if len(self.indicators[ind][sub_ind]) != 0:
-                        for t in self.indicators[ind][sub_ind]:
-                            temp_t = data_dict[ind][sub_ind][t]
-                            temp_ind.loc[~pd.isnull(temp_t)] = temp_t.loc[~pd.isnull(temp_t)]
-                    if multiple_subs:
-                        temp_data[sub_ind] = temp_ind.values
-            if not multiple_subs:
-                temp_data[ind] = temp_ind.values
-        self.data = self.data.append(temp_data)
-
-    def extract_the_data(self, elems_df, level, indic):
-        presets = {"0": ("pok", "absent"), "1": ("prizn", "PADDING-LEFT: 10pt;"), "2": ("prizn", "PADDING-LEFT: 20pt;")}
-        temp_obj = elems_df.loc[(elems_df["class"] == presets[level][0]) & (elems_df["style"] == presets[level][2])]
-        temp_df = pd.DataFrame()
-        for row in range(len(temp_obj)):
-            mapping = [True if i in temp_obj.text[row] else False for i in indic.keys()]
-            if sum(mapping) == 0:
-                continue
-            elif len(indic[indic.keys()[mapping]]) == 0:
-                self.data = self.data.append(pd.DataFrame({}))
-            temp_df = temp_df.append(pd.DataFrame({"indicator": pd.Series(np.array(indic.keys())[mapping])
-                                                   if sum(mapping) > 0 else np.NaN,
-                                                   "start": temp_obj.index[row],
-                                                   "end": pd.Series(temp_obj.index[row + 1]) if row != len(temp_obj) - 1
-                                                   else pd.Series(len(temp_obj))}))
+        for ind in self.get_indicators():
+            values = data.loc[(data.indicator == ind[0]) & (data.sub_indicator == ind[1]) & (data.times == ind[2]),
+                              present_years].values[0]
+            unit = data.loc[(data.indicator == ind[0]) & (data.sub_indicator == ind[1]) & (data.times == ind[2]),
+                            "unit"].values
+            another_temp_data = pd.DataFrame({"indicator": pd.Series(itertools.repeat(ind[0], len(present_years))),
+                                              "sub_indicator": pd.Series(itertools.repeat(ind[1], len(present_years))),
+                                              "times": pd.Series(itertools.repeat(ind[2], len(present_years))),
+                                              "values": values,
+                                              "unit": pd.Series(itertools.repeat(unit[0], len(present_years)))})
+            self.data = self.data.append(pd.concat([temp_data, another_temp_data], axis=1))
 
 
 if __name__ == "__main__":
-    Container(regions=["Амурская область", "Кировская область", "Липецкая область", "Мурманская область",
-                       "Костромская область", "Республика Алтай", "Республика Марий Эл", "Хабаровский край",
-                       "Тульская область"],
-              indicators={"расходы бюджета": {"дорожное хозяйство (дорожные фонды)": {},
-                                              "жилищно-коммунальное хозяйство": {},
-                                              "образование": {}}},
+    Container(regions=["Приморский край"],
+              indicators={"доля протяженности автодорог общего пользования местного значения, не отвечающих": {"Not present": ["Not present"]},
+                          "не имеющих регулярного автобусного": {"Not present": ["Not present"]},
+                          "доля муниципальных дошкольных образовательных учреждений, здания которых находятся в аварийном состоянии": {"Not present": ["Not present"]},
+                          "объем незавершенного в установленные сроки строительства": {"Not present": ["Not present"]},
+                          "состоящего на учете в качестве нуждающегося в жилых помещениях": {"Not present": ["Not present"]},
+                          "число спортивных сооружений": {"Not present": ["Not present"]},
+                          "заменено и отремонтировано уличной газовой сети": {"Not present": ["Not present"]},
+                          "общая протяженность улиц, проездов, набережных (на конец года)": {"Not present": ["Not present"]},
+                          "общая протяженность освещенных частей улиц, проездов, набережных (на конец года)": {"Not present": ["Not present"]},
+                          "протяженность тепловых и паровых сетей, которые были заменены и отремонтированы": {"Not present": ["Not present"]},
+                          "протяжение уличной канализационной сети, которая заменена": {"Not present": ["Not present"]},
+                          "оценка численности населения": {"все население": {"на 1 января"}},
+                          "протяженность тепловых и паровых сетей в двухтрубном исчислении": {"Not present": ["Not present"]},
+                          "одиночное протяжение уличной водопроводной сети, которая заменена и отремонтирована": {"Not present": ["Not present"]},
+                          "одиночное протяжение уличной водопроводной сети": {"уличная водопроводная сеть": ["Not present"]},
+                          "одиночное протяжение уличной канализационной сети, которая заменена и отремонтирована": {"Not present": ["Not present"]}},
               years=["2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"])
+
+
+#if __name__ == "__main__":
+#    Container(regions=["Приморский край"],
+#              indicators={"расходы бюджета": {"дорожное хозяйство (дорожные фонды)": ["Not present"],
+#                                              "жилищно-коммунальное хозяйство": ["Not present"],
+#                                              "образование": ["Not present"]}},
+#              years=["2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"])
